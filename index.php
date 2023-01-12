8b0efbe11d90571df730df41529c65a1096f4fcb
Mckingsley <44697561+Mckingsley@users.noreply.github.com>
2023-01-12 02:42:31 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./styles.css">
+	<link rel="stylesheet" type="text/css" href="./media.css">
+	<link rel="stylesheet" type="text/css" href="./fontawesome-web/css/all.min.css">
+	<title>AGROKOM</title>
+</head>
+<body>
+
+	<div class="body">
+
+		
+		<div class="garde">
+
+			<div class="logo_garde">
+				<a href="/Agrokom/"><img src="/Agrokom/images/logos/LOGO_AGROKOM00.png" alt="logo" ></a>
+				
+			</div>
+
+			<div class="text" >
+				<p>
+					Expert et pionnier dans la production du tourteau et de l’huile de soja biologique au Togo et en Afrique de l’ouest...
+				</p>
+				<a href="/Agrokom/home.php"> <span > ACCEUIL </span></a>
+
+			</div>
+
+			<div class="produits">
+				<div class="soja" onclick=window.location.href='/Agrokom/pages/f_soja.php'> <p><a href="/Agrokom/pages/f_soja.php" >FILIÈRE SOJA</a></p></div>
+				<div class="sesame" onclick=window.location.href='/Agrokom/pages/f_sesame.php'><p><a href="/Agrokom/pages/f_sesame.php">FILIÈRE SESAME</a></p></div>
+				<div class="karite" onclick=window.location.href='/Agrokom/pages/f_karite.php'> <p><a href="/Agrokom/pages/f_karite.php">FILIÈRE KARITE</a></p></div>
+				<div class="fonio" onclick=window.location.href='/Agrokom/pages/f_fonio.php'> <p><a href="/Agrokom/pages/f_karite.php">FILIÈRE FONIO</a></p></div>
+			</div>
+		</div>
+
+
+			
+		</div>
+
+		
+
+		
+	</div>
+
+
+
+</body>
+</html>
---
+++ logo.php
@@ -0,0 +1,16 @@
+!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>logo</title>
+</head>
+<body>
+
+	<div class="logo">
+			<img src="./agro_logo-11.png" alt="logo">
+			<div class="slogan"> ORGANIC AGRICULTURE AND FAIR TRADE</div>
+		</div>
+
+</body>
+</html>
